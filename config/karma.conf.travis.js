dde756e3f12c2e9fb0314a7e5615930b1808be59
fredkingham <fredkingham@gmail.com>
2016-03-31 12:31:01 +0100

    removed the unneeded karma file

--- config/karma.conf.travis.js
+++ config/karma.conf.travis.js
@@ -1,12 +1,21 @@
 module.exports = function(config){
+    var browsers, basePath;
+
+    if(process.env.TRAVIS){
+        browsers = ["Firefox"];
+        basePath = '/home/travis/virtualenv/python2.7/src/opal/opal/static/js';
+    }
+    else{
+        browsers = ['PhantomJS'];
+        basePath = '../../opal/opal/static/js';
+    }
+
     config.set({
         frameworks: ['jasmine'],
-        browsers: ['Firefox'],
-        basePath:  '/home/travis/virtualenv/python2.7/src/opal/opal/static/js',
+        browsers: browsers,
+        basePath:  basePath,
 
         files: [
-            //JASMINE,
-            //JASMINE_ADAPTER,
             //JASMINE,
             //JASMINE_ADAPTER,
             'angular-1.2.20/angular.js',
@@ -14,9 +23,11 @@ module.exports = function(config){
             'angular-1.2.20/angular-resource.js',
             'angular-1.2.20/angular-cookies.js',
             'angular-1.2.20/angular-mocks.js',
+
             'angular-ui-utils-0.1.0/ui-utils.js',
             'angular-ui-bootstrap-0.10.0/ui-bootstrap-tpls.js',
-
+            'angular-strap-2.3.1/angular-strap.js',
+            'angular-strap-2.3.1/modules/compiler.js',
             'angular-strap-2.3.1/modules/tooltip.js',
             'angular-strap-2.3.1/modules/tooltip.tpl.js',
             'angular-strap-2.3.1/modules/dimensions.js',
@@ -28,24 +39,29 @@ module.exports = function(config){
             'angular-strap-2.3.1/modules/timepicker.tpl.js',
             'angular-strap-2.3.1/modules/typeahead.js',
             'angular-strap-2.3.1/modules/typeahead.tpl.js',
+            "angulartics-0.17.2/angulartics.min.js",
+            "angulartics-0.17.2/angulartics-ga.min.js",
             'ngprogress-lite/ngprogress-lite.js',
-            'jquery-1.11.0/jquery.js',
+            'jquery-1.11.3/jquery-1.11.3.js',
             'utils/underscore.js',
+            'utils/showdown.js',
             'utils/moment.js',
             'bower_components/angular-growl-v2/build/angular-growl.js',
             'bower_components/ment.io/dist/mentio.js',
             'bower_components/ment.io/dist/templates.js',
-
+            'bower_components/angular-ui-select/dist/select.js',
             'opal/utils.js',
+            'opal/opaldown.js',
+            'opal/directives.js',
+            'opal/filters.js',
             'opal/services_module.js',
             'opal/services/*.js',
             'opal/services/flow.js',
             'opal/controllers_module.js',
             'opal/controllers/*.js',
-
-            __dirname+'/../dashboard/static/js/dashboard/*.js',
-            __dirname+'/../dashboard/static/js/dashboard/controllers/*.js',
-            __dirname+'/../dashboard/static/js/test/*.js',
+             __dirname+'/../dashboard/static/js/dashboard/*.js',
+             __dirname+'/../dashboard/static/js/dashboard/controllers/*.js',
+             __dirname+'/../dashboard/static/js/test/*.js',
         ],
 
         // Stolen from http://oligofren.wordpress.com/2014/05/27/running-karma-tests-on-browserstack/
@@ -53,7 +69,15 @@ module.exports = function(config){
         browserDisconnectTolerance : 1, // default 0
         browserNoActivityTimeout : 4*60*1000, //default 10000
         captureTimeout : 4*60*1000, //default 60000
+        preprocessors: {
+            'opal/**/*.js': 'coverage',
+            '../../core/search/static/js/search/**/*.js': 'coverage',
+        },
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {
+            type: 'lcovonly', // lcov or lcovonly are required for generating lcov.info files
+            dir: '../../../coverage/',
+        }
 
-
-    })
-}
+    });
+};
