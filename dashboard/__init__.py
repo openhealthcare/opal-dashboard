50b14684c25d38cb92e4414348b1dcf4bf502bcc
David Miller <david@deadpansincerity.com>
2016-11-27 11:45:40 +0000

    We may as well use the opal.core.plugin definitions for the methods that still exist

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -48,32 +48,6 @@ class DashboardPlugin(plugins.OpalPlugin):
         )
     ]
 
-    def restricted_teams(self, user):
-        """
-        Return any restricted teams for particualr users that our
-        plugin may define.
-        """
-        return []
-
-    def list_schemas(self):
-        """
-        Return any patient list schemas that our plugin may define.
-        """
-        return {}
-
-    def flows(self):
-        """
-        Return any custom flows that our plugin may define
-        """
-        return {}
-
-    def roles(self, user):
-        """
-        Given a (Django) USER object, return any extra roles defined
-        by our plugin.
-        """
-        return {}
-
 plugins.register(DashboardPlugin)
 
 
