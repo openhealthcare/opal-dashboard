8d4d0c757d9d1ec79cd4366da7723bad02192197
David Miller <david@deadpansincerity.com>
2016-11-27 10:18:12 +0000

    Add some actual tests for widgets

--- dashboard/tests/test_widgets.py
+++ dashboard/tests/test_widgets.py
@@ -1,5 +1,17 @@
 """
 Unittests for dashboard
 """
-from opal.core.test import TestCase
+from opal.core.test import OpalTestCase
 
+from dashboard import widgets
+
+class TestWidget(widgets.Widget):
+    tagline = 'teh tagline'
+
+class WidgetTestCase(OpalTestCase):
+
+    def test_get_tagline(self):
+        self.assertEqual('teh tagline', TestWidget().get_tagline())
+
+    def test_get_bg(self):
+        self.assertEqual('active', widgets.Widget().get_bg())
