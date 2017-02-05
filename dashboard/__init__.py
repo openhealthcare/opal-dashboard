60e5f34f31a87a6796748876eb309ecf6d353aa0
David Miller <david@deadpansincerity.com>
2017-02-05 18:35:15 +0000

    Kill old plugin register / import

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from opal.utils import stringport, camelcase_to_underscore
 
 from opal.core import discoverable
-from dashboard import plugin # Don't do this once we've updated plugins in Opal
 
 class Dashboard(discoverable.DiscoverableFeature):
     module_name = 'dashboards'
