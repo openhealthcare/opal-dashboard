88521bd26a057944d331338e95ecc122c65a3055
fredkingham <fredkingham@gmail.com>
2018-01-03 10:46:31 +0000

    fix travis and karma

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,20 +1,13 @@
 module.exports = function(config){
-  var opalPath;
-  if(process.env.TRAVIS){
-    python_version = process.env.TRAVIS_PYTHON_VERSION;
-    opalPath = '/home/travis/virtualenv/python' + python_version + '/src/opal';
-  }
-  else{
-    opalPath = '../../opal';
-  }
-  var karmaDefaults = require(opalPath + '/config/karma_defaults.js');
+  var opalPath = process.env.OPAL_LOCATION;
+  var karmaDefaults = require(opalPath + '/opal/tests/js_config/karma_defaults.js');
   var baseDir = __dirname + '/..';
   var coverageFiles = [
     __dirname + '/../dashboard/static/js/dashboard/*.js',
   ];
   var includedFiles = [
-      __dirname + '/../dashboard/static/js/dashboard/*.js',
-      __dirname + '/../dashboard/static/js/test/*.js',
+    __dirname + '/../dashboard/static/js/dashboard/*.js',
+    __dirname + '/../dashboard/static/js/test/*.js',
   ];
 
   var defaultConfig = karmaDefaults(includedFiles, baseDir, coverageFiles);
